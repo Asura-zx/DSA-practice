b7d177c601808e617d0dfb10b2dcbe0454ba71d2
Aayush Lamsal <lamsal.aayush123@gmail.com>
2024-01-14 10:47:11 +0545

    recovery

--- LInked List/singlelinkedlist/single_liner_linked_list/main.c
+++ LInked List/singlelinkedlist/single_liner_linked_list/main.c
@@ -1,125 +1,117 @@
-#include <stdio.h>
-#include <stdlib.h>
+//linked list 
+#include<stdio.h>
+#include<stdlib.h>
 
-
-//for some reason using it caused major execution delay..should investigate...
-/*
-#ifdef _WIN32
-#define clear system("cls")
-#define pause system("pause")
-
-#else 
-#define clear system("clear")
-#define pause system("read -n 1 -s -p \"\"")
-
-#endif
-*/
-struct list{
+//create structure for node
+struct Node{
 	int info;
-	struct list *next;
+	struct Node* next;
 };
 
-/*alternatively
-	struct node{
-	...
-	};
-	struct node *start;
-*/
+typedef struct Node node;
 
-typedef struct list node;
-node *start, *item;
-int main()
-{
-	node *temp;
-	char c;
-	int n,k;
-	
-	printf("THIS IS A SINGLE LINKED LIST IMPLEMENTATION IN C\n\n");
-	printf("1. insert-at-beginning  2. insert-at-end  3. insert-at-nth-position  4. delete-front  5. delete-end  6. delete-nth-node  7. exit\n");
-	printf("Type the operation: ");
-	c=getchar()-'0';
-	getchar();
+//point start node to NULL
+node *start=NULL;
 
+
+//inserts node at beginning 
+void f_insertnode(node* i, int d){
+	i=(node*)malloc(sizeof(node));
+		i->next=start;
+		i->info=d;
+		start=i;
+}
+//insert node at last
+void l_insertnode(node* i, int d){
+	i=(node*)malloc(sizeof(node));
+	node* temp=(node*)malloc(sizeof(node));
+	i->next=NULL;
+	i->info=d;
+	temp=start;
+	if (start==NULL){
+		start=i;
+	}
+
+	while(temp->next!=NULL){
+		temp=temp->next;
+	}
+	temp->next=i;
+	//free(temp);
+}
+node* n_insertnode(node* i, int n, int d){
+	i=(node*)malloc(sizeof(node));
+	i->info=d;
+	int c=1;
+	if(n==1){
+		i->next=start;
+		start=i;
+	}
+	node* temp=(node*)malloc(sizeof(node));
+		temp=start;
+		while(c!=(n-1)){
+			temp=temp->next;
+			c++;
+		}
+		i->info=temp->info;
+		temp->next=i;
+
+	}
+//delete node
+int f_deletenode(int d){
+	return d;
+}
+int l_deletenode(int d){
+	return d;
+}
+int n_deletenode(int n, int d){
+	return d;
+}
+
+//search for data
+int searchnode(int n){
+	return n;
+}
+
+void display(){
+	node* temp;
+	temp=start;
+	if(start==NULL){
+		printf("EMPTY LIST!\n");
+	}
+		while(temp->next!=NULL){
+		printf("%d ", temp->info);
+		temp=temp->next;
+	}
+		printf("%d ", temp->info);
+	}
+
+
+
+int main(){
+	int data;
+	int c;
+	node* item;
+	printf("WELCOME TO THE LINKED LIST IMPLEMENTATION IN C\n");
+	scanf("%d", &c);
 	do{
 		switch(c){
-			case 1:
-				item=(node*)malloc(sizeof(node));
-				printf("Type the integer to insert: ");
-				scanf("%d",&(item->info));
-				
-					item ->next=start;
-					start=item;
-				
-				printf(">>>>added item at the beginning !\n");
-				break;
-
-			case 2:
-				
-				item=(node*)malloc(sizeof(node));
-				item->next=NULL;
-				temp=start;
-				printf("Enter the integer: ");
-				scanf("%d",&item->info);
-				if (start==NULL){
-					start=item;
-					item->next=NULL;
-				}
-				while(temp->next!=NULL){
-					temp=temp->next;
-				}
-				temp->next=item;
-				printf(">>>>item added at last!\n");
-				break;
-			case 3:
-				item=(node*)malloc(sizeof(node));
-				temp=start;
-				printf("Enter the position(starting from 1) and information: ");
-				scanf("%d %d",&n, &item->info);//
-				if(n==1){
-					item->next=start;
-					start=item;//if used start-> next, next item points to start and next node of start points to item creating infinte loop
-				}
-				else{
-					k=1;
-					while(temp!=NULL && k<n-1){
-						temp=temp->next;
-						k++;
-					}
-					if (temp==NULL){
-						fprintf(stderr, "reached the end of list\n");
-						free(temp);
-					}
-					else{
-						item->next=temp->next;//temp->next node<-item
-						temp->next=item;// temp->item->next node
-					}
-				}
-				printf(">>>>item added at %d position!\n",n);
-				break;
-			case 4:
-				
-			case 8:
-				temp=start;
-				k=0;
-				printf("###########  ");
-				do{
-					printf("%d ", temp->info);
-					//printf("loop started");
-					temp=temp->next;
-
-				}while(temp!=NULL);
-				printf("\n\n");
-				break;
-			default:
-				printf("\ndefault statement\n");
-				break;
+		case 1:
+			printf("Add data to add at the beginning of list: ");
+			scanf("%d",&data);
+			f_insertnode(item, data);
+			printf("Data added!");
+			break;
+		case 2:
+			printf("Add data to add at the end of list: ");
+			scanf("%d", &data);
+			l_insertnode(item, data);
+			printf("Data added!");
+			break;
+		default:
+			display();
+			break;
 		}
-
-
-	getchar();
-	printf("1. insert-at-beginning  2. insert-at-end  3. insert-at-nth-position  4. delete-front  5. delete-end  6. delete-nth-node  7. exit\n");
-	printf("Type the opearation: ");
-	c=getchar()-'0';
-	}while(c!=7);
+		scanf("%d", &c);
+	}while(c!=0);
 	return 0;
-}
+} 
